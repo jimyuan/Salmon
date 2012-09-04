7bea412acdaa1a59605c612497a361f50a8320dc
Jim Yuan <jim@home.com>
2012-09-04 23:12:55 +0800

    render homepage gallery success

--- build/index.js
+++ build/index.js
@@ -22,11 +22,28 @@ fs.readdirSync(__dirname + '/../templates/pages').forEach(function (name) {
 	if (!name.match(/\.mustache$/)) return
 
 	var page = fs.readFileSync(__dirname  + '/../templates/pages/' + name, 'utf-8');
-	var context ={page : true}
-	//context[name.replace(/\.mustache$/, '')] = ' class="active"';
+	var context ={};
+	//set active page
 	var key=name.replace(/\.mustache$/, '').split("-");
 	context[key[0]]= ' class="active"';
 	if(key.length==2) context[key[1]]= ' class="active"';
+
+	//set homepage variable
+	if(name.match(/^index/)) {
+		context["items"]=[
+			 {"img":"Marinated-Salmon-Portion", "name":"调味三文鱼块", "active": " active"}
+			,{"img":"Salmon-Skewer", "name":"烧烤三文鱼串"}
+			,{"img":"Light-Salted-Salmon", "name":"盐烧三文鱼"}
+			,{"img":"Stuffed-Salmon", "name":"三文鱼芝士夹心"}
+			,{"img":"Graved-Salmon", "name":"香草三文鱼"}
+			,{"img":"Salmon-Sashimi", "name":"三文鱼刺身"}
+			,{"img":"Salmon-Burger", "name":"三文鱼饼"}
+			,{"img":"Salmon-Sausage", "name":"三文鱼香肠"}
+		];
+		context["jszone"]=true;
+		context["script"]=key[0]
+	}
+
 	page = hogan.compile(page)
 	page = layout.render(context, {
 		body: page
